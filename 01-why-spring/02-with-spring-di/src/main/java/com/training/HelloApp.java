97aed77bf8ec38765f156c5000b4337bb961a9e7
Assanai Manurat <assanai17@gmail.com>
2017-08-25 16:43:48 +0700

    Update

--- 01-why-spring/02-with-spring-di/src/main/java/com/training/HelloApp.java
+++ 01-why-spring/02-with-spring-di/src/main/java/com/training/HelloApp.java
@@ -10,12 +10,10 @@ public class HelloApp {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
         Hello hello = context.getBean(Hello.class);
+        System.out.println(hello.getName());
 
         FileSystemXmlApplicationContext fileSystemXmlApplicationContext =
                 new FileSystemXmlApplicationContext("//Users/amanurat/Traning/Courses/Spring-Practices/training-spring-practices/01-why-spring/02-with-spring-di/src/main/resources/spring-config.xml");
-
-
-        System.out.println(hello.getName());
 
         Hello hello2 = fileSystemXmlApplicationContext.getBean(Hello.class);
         System.out.println(hello2.getName());
