cd08831570b2d94adc25edcad6babbce2a3b2e4b
Assanai Manurat <assanai17@gmail.com>
2017-08-26 05:47:15 +0700

    Update Replace code with //TODO

--- 01-why-spring/02-with-spring-di/src/main/java/com/training/HelloApp.java
+++ 01-why-spring/02-with-spring-di/src/main/java/com/training/HelloApp.java
@@ -1,22 +1,12 @@
 package com.training;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
-
 public class HelloApp {
 
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
-        Hello hello = context.getBean(Hello.class);
-        System.out.println(hello.getName());
-
-        FileSystemXmlApplicationContext fileSystemXmlApplicationContext =
-                new FileSystemXmlApplicationContext("//Users/amanurat/Traning/Courses/Spring-Practices/training-spring-practices/01-why-spring/02-with-spring-di/src/main/resources/spring-config.xml");
-
-        Hello hello2 = fileSystemXmlApplicationContext.getBean(Hello.class);
-        System.out.println(hello2.getName());
+        //TODO load application context with classpath from XML Configuration File
+        //TODO get bean Hello
+        //TODO load application context with file system from XML Configuration File
 
     }
 
